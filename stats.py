ead9c0c047806987d21aa027800cac8a19d0be56
Tyler Westerberg <tylerwesterberg05@gmail.com>
2024-08-04 21:05:50 -0400

    added getHittingStats and some random tests

--- stats.py
+++ stats.py
@@ -1,9 +1,7 @@
 import statsapi, datetime, json, pprint
 
-# for player in statsapi.lookup_player('Judge,'):
-#     print('Full name: {}, Position: {}'.format(player['fullName'], player['primaryPosition']['abbreviation']))
-
 class Player:
+    player_stats = None
     def __init__(self, name, id, position):
         self.name = name
         self.id = id
@@ -11,8 +9,22 @@ class Player:
 
     def __str__(self):
         return f"{self.name}"
+    
+    def getHittingStats(self):
+        if (self.player_stats == None):
+            self.player_stats = statsapi.player_stat_data(self.id, group='hitting', type="career", sportId=1)['stats'][0]['stats']
+        return self.player_stats
+
+    def getID(self):
+        return self.id
+    
+    def getName(self):
+        return self.name
 
 class Team:
+    name: str
+    id: str
+    players: list[Player]
     def __init__(self, name, id, players):
         self.name = name
         self.id = id
@@ -23,17 +35,28 @@ class Team:
         for player in self.players:
             playersString += (str(player) + " ")
         return f"{self.name}: {playersString}" 
+    
+    def getPlayers(self):
+        return self.players
 
-teamJSON = statsapi.get('teams', {'sportId':1})
 
-teams = []
+def getTeams():
+    teamJSON = statsapi.get('teams', {'sportId':1})
 
-for team in teamJSON['teams']:
-    players = []
-    for player in statsapi.get('team_roster', {'teamId':team['id']})['roster']:
-        players.append(Player(player['person']['fullName'], player['person']['id'], player['position']['abbreviation']))
+    teams = []
 
-    teams.append(Team(team['teamName'], team['id'], players))
+    for team in teamJSON['teams']:
+        players = []
+        for player in statsapi.get('team_roster', {'teamId':team['id']})['roster']:
+            players.append(Player(player['person']['fullName'], player['person']['id'], player['position']['abbreviation']))
 
-for team in teams:
-    print(team)
+        teams.append(Team(team['teamName'], team['id'], players))
+
+    return teams
+
+for team in getTeams():
+    for player in team.getPlayers():
+        if player.getName() == "Aaron Judge":
+            print(player.getID())
+            pprint.pprint(player.getHittingStats())
+
